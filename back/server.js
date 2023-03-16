8bfbb72f0f69f63b2abb5315c18d7f889c393cc7
VladGodOfJs <vladka910@gmail.com>
2023-03-16 17:53:35 +0200

    add nodemon

--- back/server.js
+++ back/server.js
@@ -1,22 +1,27 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import express from "express";
+import morgan from "morgan";
 
 import authRoutes from "./app/auth/auth.routes.js";
 
+dotenv.config()
+
 const app = express();
 
-async  function main(){
- if(process.env.NODE_ENV === 'development')app.use(morgan
- ('dev'))
- app.use(express.json());
- app.use('/api/auth',authRoutes);
+async function main() {
+    if (process.env.NODE_ENV === 'development') app.use(morgan
+    ('dev'))
+    app.use(express.json());
+    app.use('/api/auth', authRoutes);
 
- const PORT = 5000
+    const PORT = process.env.PORT || 5000
 
- app.listen(
-     PORT,
-     console.log(
-         `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-     )
- )
+    app.listen(
+        PORT,
+        console.log(
+            `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+        )
+    )
 }
+
 main()
