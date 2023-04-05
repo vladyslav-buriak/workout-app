54cf39885ee1451e704b528d8873147bfd5a450a
vladyslav-buriak <vladka910@gmail.com>
2023-04-05 18:54:19 +0300

    add protect exercisesRoutes

--- back/app/exercises/exercisesRoutes.js
+++ back/app/exercises/exercisesRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import { addExercises } from './exercisesController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
-router.route('/').post( addExercises)
+router.route('/').post(protect, addExercises)
 
 export default router
