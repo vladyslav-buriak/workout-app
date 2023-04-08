9c606ce15adca934e14e6a3752120df972c69193
vladyslav-buriak <vladka910@gmail.com>
2023-04-08 22:33:19 +0300

    add delete and update

--- back/app/exercises/exercisesController.js
+++ back/app/exercises/exercisesController.js
@@ -1,10 +1,10 @@
 import expressAsyncHandler from "express-async-handler";
-import {prisma} from "../prisma.js";
+import { prisma } from "../prisma.js";
 
 export const addExercises = expressAsyncHandler(async (req, res) => {
 
 
-    const{times ,name, iconPath } = req.body
+    const { times, name, iconPath } = req.body
 
     const exercises = await prisma.exercises.create({
 
@@ -29,3 +29,50 @@ export const getExercises = expressAsyncHandler(async (req, res) => {
 
 }
 )
+
+
+export const updateExercise = expressAsyncHandler(async (req, res) => {
+    const { name, times, iconPath } = req.body
+
+    try {
+        const exercises = await prisma.exercises.update({
+            where: {
+                id: +req.params.id
+            },
+            data: {
+                name,
+                times,
+                iconPath
+            }
+        })
+
+        res.json(exercises)
+    } catch (error) {
+        res.status(404)
+        throw new Error('Exercise not found!')
+    }
+})
+
+
+export const deleteExercises = expressAsyncHandler(async (req, res) => {
+
+
+    try {
+         await prisma.exercises.delete({
+            where: {
+                id: +req.params.id
+            },
+        })
+
+        res.json({ message: 'deleted' })
+    } catch (error) {
+        res.status(404)
+        throw new Error('Exercise not found!')
+    }
+})
+
+
+
+
+
+
--- back/app/exercises/exercisesRoutes.js
+++ back/app/exercises/exercisesRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { addExercises, getExercises } from './exercisesController.js';
+import { addExercises, deleteExercises, getExercises, updateExercise,  } from './exercisesController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 router.route('/').post(protect, addExercises)
 router.route('/').get(protect, getExercises)
+router.route('/:id').put(protect, updateExercise).delete(protect ,deleteExercises)
+
+
+
+
 
 export default router
